b26abcd58054862c6a1ea6414c62417ddd4815be
Chris Daniels <danielschris96@gmail.com>
2023-04-28 15:56:06 -0700

    added dependencies and new database with tables

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS business_db;
+CREATE DATABASE business_db;
+
+USE business_db;
+
+CREATE TABLE department (
+    id INT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salaey DECIMAL,
+    department_id INT
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+);
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+  
