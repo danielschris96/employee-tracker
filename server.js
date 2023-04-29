8efdc2490f255cdd956dcaa2b2a8075924e6b50a
Chris Daniels <danielschris96@gmail.com>
2023-04-28 17:49:01 -0700

    worked on inquirer bug

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cTable = require('console.table');
+const util = require('util');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -18,37 +19,45 @@ const db = mysql.createConnection(
   console.log(`Connected to the courses_db database.`)
 );
 
-function viewDepartments() {
+// const query = util.promisify(db.query).bind(db);
 
-}
+// function viewDepartments() {
+// console.log('hello')
+// }
 
-function viewRoles() {
+// function viewRoles() {
+//   console.log('hello')
+// }
 
-}
+// function viewEmployees() {
+//   query('SELECT * FROM employees')
+//   .then(rows => {
+//     console.table(rows);
+//   })
+//   .catch(err => {
+//     console.error(err);
+//   });
+// }
 
-function viewEmployees() {
+// function addDepartment() {
+//   console.log('hello')
+// }
 
-}
+// function addRole() {
+//   console.log('hello')
+// }
 
-function addDepartment() {
+// function addEmployee() {
+//   console.log('hello')
+// }
 
-}
-
-function addRole() {
-
-}
-
-function addEmployee() {
-
-}
-
-function updateRole() {
-
-}
+// function updateRole() {
+//   console.log('hello')
+// }
 
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+//   });
   
-  module.exports = { viewDepartments, viewRoles, viewEmployees, addDepartment, addRole, addEmployee, updateRole };
+//   module.exports = { viewDepartments, viewRoles, viewEmployees, addDepartment, addRole, addEmployee, updateRole };
