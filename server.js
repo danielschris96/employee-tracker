a21fa845cb3a135a99872edfacb7c0d34c40034c
unknown <danielschris96@gmail.com>
2023-04-30 09:40:26 -0700

    worked on functions for inquirer prompts

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const cTable = require('console.table');
 const util = require('util');
+const inquirer = require('inquirer');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -19,45 +20,106 @@ const db = mysql.createConnection(
   console.log(`Connected to the courses_db database.`)
 );
 
-// const query = util.promisify(db.query).bind(db);
+const query = util.promisify(db.query).bind(db);
 
-// function viewDepartments() {
-// console.log('hello')
-// }
+function viewDepartments() {
+  query('SELECT * FROM department')
+  .then(rows => {
+    console.table(rows);
+  })
+  .catch(err => {
+    console.error(err);
+  });
+}
 
-// function viewRoles() {
-//   console.log('hello')
-// }
+function viewRoles() {
+  query('SELECT * FROM role')
+  .then(rows => {
+    console.table(rows);
+  })
+  .catch(err => {
+    console.error(err);
+  });
+}
 
-// function viewEmployees() {
-//   query('SELECT * FROM employees')
-//   .then(rows => {
-//     console.table(rows);
-//   })
-//   .catch(err => {
-//     console.error(err);
-//   });
-// }
+function viewEmployees() {
+  query('SELECT * FROM employee')
+  .then(rows => {
+    console.table(rows);
+  })
+  .catch(err => {
+    console.error(err);
+  });
+}
 
-// function addDepartment() {
-//   console.log('hello')
-// }
+function addDepartment() {
+  inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of the department that you would like to add?',
+    },
+  ])
+  .then((answers) => {
+    const { name } = answers;
+    const sql = 'INSERT INTO departments (name) VALUES (?)';
+    query(sql, [name])
+      .then (() => {
+        console.log (`Successfully added ${name} to department list`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+};
 
-// function addRole() {
-//   console.log('hello')
-// }
+function addRole() {
+  inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the name of the role that you would like to add?',
+    },
+    {
+      type: 'input',
+      name: 'salary',
+      message: 'What is the salary for this role?',
+    },
+    {
+      type: 'list',
+      name: 'department',
+      message: 'What department does this role belong to?',
+      choices: [
+        
+      ]
+    }
+  ])
+  .then((answers) => {
+    const { title } = answers;
+    const sql = 'INSERT INTO role (title) VALUES (?)';
+    query(sql, [title])
+      .then (() => {
+        console.log (`Successfully added ${title} to department list`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+}
 
-// function addEmployee() {
-//   console.log('hello')
-// }
+function addEmployee() {
+  console.log('hello')
+}
 
-// function updateRole() {
-//   console.log('hello')
-// }
+function updateRole() {
+  console.log('hello')
+}
 
 
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-//   });
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
   
-//   module.exports = { viewDepartments, viewRoles, viewEmployees, addDepartment, addRole, addEmployee, updateRole };
+  module.exports = { viewDepartments, viewRoles, viewEmployees, addDepartment, addRole, addEmployee, updateRole };
